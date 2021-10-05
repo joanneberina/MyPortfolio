d33d1de3f535282e44ad8b4f5844689b940c6465
Dennis Padiernos <depadiernos@gmail.com>
2021-10-04 21:47:33 -0500

    update rvway case study. misc updates to case-studies-list and nav-bar to fix some styling and layout issues.

--- src/js/nav-bar.js
+++ src/js/nav-bar.js
@@ -2,14 +2,14 @@ class Navbar extends HTMLElement {
   connectedCallback() {
     this.innerHTML = /*html*/ `
     <nav>
-      <a href="/" id="home">
-        Joanne Berina
-      </a>
-      <ul id="nav-ul">
-          <li><a href="/">Work</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/assets/joanneberina_resume.pdf" target="_blank">Resume</a></li>
-      </ul>
+    <a href="/" id="home">
+    Joanne Berina
+    </a>
+    <ul id="nav-ul">
+    <li><a href="/">Work</a></li>
+    <li><a href="/about">About</a></li>
+    <li><a href="/assets/joanneberina_resume.pdf" target="_blank">Resume</a></li>
+    </ul>
     </nav>
     `
     document.querySelectorAll('#nav-ul>li>a').forEach(item => {
@@ -17,6 +17,18 @@ class Navbar extends HTMLElement {
         item.parentElement.classList.add('nav_current')
       }
     })
+
+    const scrollFunction = () => {
+      console.log('scroll')
+      if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.querySelector('nav').setAttribute('class', 'nav-background')
+      } else {
+        document.querySelector('nav').removeAttribute('class', 'nav-background')
+      }
+    }
+    window.onscroll = scrollFunction
+
+    window.addEventListener('resize', scrollFunction)
   }
 }
 
